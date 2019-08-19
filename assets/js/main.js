38fc04c56a5c6648144287419141750fef0c3502
mwilliams <mwilliams@azavea.com>
2019-08-19 11:58:05 -0400

    updates homepage based on feedback

--- assets/js/main.js
+++ assets/js/main.js
@@ -372,16 +372,21 @@
       url: cfg.mailChimpURL
     });
 
-    // Mailchimp translation
-    //
-    //  Defaults:
-    //	 'submit': 'Submitting...',
-    //  0: 'We have sent you a confirmation email',
-    //  1: 'Please enter a value',
-    //  2: 'An email address must contain a single @',
-    //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-    //  4: 'The username portion of the email address is invalid (the portion before the @: )',
-    //  5: 'This email address looks fake or invalid. Please enter a real email address'
+    $.ajaxChimp.translations.es = {
+      submit: "Submitting...",
+      0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+      1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+      2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+      3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+      4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+      5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+    };
+  };
+  var clAjaxChimpFooter = function() {
+    $("#mc-form-footer").ajaxChimp({
+      language: "es",
+      url: cfg.mailChimpURL
+    });
 
     $.ajaxChimp.translations.es = {
       submit: "Submitting...",
@@ -429,6 +434,7 @@
     clContactForm();
     clAOS();
     clAjaxChimp();
+    clAjaxChimpFooter();
     clBackToTop();
   })();
 })(jQuery);
